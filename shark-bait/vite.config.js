59351fa7eaf3046bbe7d6b94b4931dedca80dbe8
sebgutierrez <84500161+sebgutierrez@users.noreply.github.com>
2025-10-07 16:49:21 -0500

    deleted references of cesiumjs

--- shark-bait/vite.config.js
+++ shark-bait/vite.config.js
@@ -6,30 +6,11 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'node_modules/@cesium/engine/Source/Assets',
-          dest: 'cesium'
-        },
-        {
-          src: 'node_modules/@cesium/engine/Source/ThirdParty',
-          dest: 'cesium'
-        },
-        {
-          src: 'node_modules/@cesium/engine/Source/Workers',
-          dest: 'cesium'
-        }
-      ]
-    })
+    react()
   ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
-  define: {
-    CESIUM_BASE_URL: JSON.stringify('/cesium')
-  }
 })
